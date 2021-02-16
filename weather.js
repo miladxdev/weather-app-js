182908723a081af1fb5c699c132c5e532a35cbe2
milad <miladxgh@outlook.com>
2021-02-16 14:30:26 +0330

    fetch data

--- weather.js
+++ weather.js
@@ -1,4 +1,10 @@
 let weather = {
 
-    "apiKey": "359f0831c53fa20ed2ff23f00ae0904e"
-}
+    apiKey: "359f0831c53fa20ed2ff23f00ae0904e",
+
+    fetchWeather: function () {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=esfahan&units=metric&appid=359f0831c53fa20ed2ff23f00ae0904e")
+        .then(response => response.json())
+        .then(data => console.log(data));
+    }
+};
