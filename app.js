45ef83b9363b89ab0b88fcba4bf2afcdf572ee7d
Milad <miladxgh@outlook.com>
2021-06-10 17:17:45 +0430

    animate new data

--- app.js
+++ app.js
@@ -1,55 +1,67 @@
-import { displayLoading, hideLoading } from "./script.js";
+import { displayLoading, hideLoading, errorText, animateNewData } from "./script.js";
 
 let weather = {
+  apiKey: "359f0831c53fa20ed2ff23f00ae0904e",
 
-    apiKey: "359f0831c53fa20ed2ff23f00ae0904e",
+  fetchWeather(city) {
+    displayLoading();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data))
+      .catch((error) => this.handleErrors(error));
+  },
 
-    fetchWeather(city) {
-        displayLoading();
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
-            .then(response => response.json())
-            .then(data => this.displayWeather(data))
-            .catch(error => alert(error));
-    },
+  displayWeather(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, temp_min, temp_max, feels_like, humidity, pressure } = data.main;
+    const { speed } = data.wind;
+    const { country } = data.sys;
 
-    displayWeather(data) {
-        hideLoading();
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, temp_min, temp_max, feels_like, humidity, pressure } = data.main;
-        const { speed } = data.wind;
-        const { country } = data.sys;
+    document.querySelector(".city").innerText = name;
+    document.querySelector(".temp").innerText = `${Math.floor(temp)}°C`;
+    document.querySelector("#w-icon").src = `http://openweathermap.org/img/wn/${icon}@4x.png`;
+    document.querySelector("#feels-like").innerText = feels_like + " °C";
+    document.querySelector("#country").innerText = country;
+    document.querySelector("#humidity").innerText = humidity + "%";
+    document.querySelector("#wind").innerText = speed + " km/h";
+    document.querySelector("#pressure").innerText = pressure;
+    document.querySelector("#temp-min").innerText = temp_min + "°C";
+    document.querySelector("#temp-max").innerText = temp_max + "°C";
+    document.querySelector("#description").innerText = description;
+    // document.body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/daily?${name})`; //unsplash API
+    hideLoading();
+    animateNewData();
+  },
 
-        document.querySelector(".city").innerText = name;
-        document.querySelector(".temp").innerText = `${Math.floor(temp)}°C`;
-        document.querySelector("#w-icon").src = `http://openweathermap.org/img/wn/${icon}@4x.png`;
-        document.querySelector("#feels-like").innerText = feels_like + ' °C';
-        document.querySelector("#country").innerText = country;
-        document.querySelector("#humidity").innerText = humidity + "%";
-        document.querySelector("#wind").innerText = speed + ' km/h';
-        document.querySelector("#pressure").innerText = pressure;
-        document.querySelector("#temp-min").innerText = temp_min + '°C';
-        document.querySelector("#temp-max").innerText = temp_max + '°C';
-        document.querySelector("#description").innerText = description;
-        // document.body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/daily?${name})`; //unsplash API
-    },
+  search() {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 
-    search() {
-        this.fetchWeather(document.querySelector(".search-bar").value);
+  handleErrors(error) {
+    if (!navigator.onLine) {
+      errorText("error: you are offline");
+    } else if (document.querySelector(".search-bar").value == "") {
+      errorText("error: search bar is empty");
+    } else {
+      errorText("error: can't get data");
     }
+  },
 };
 
-document.querySelector(".search button")
-    .addEventListener("click", () => weather.search());
+document.querySelector(".search button").addEventListener("click", () => weather.search());
 
-
-document.querySelector(".search-bar")
-    .addEventListener("keyup", event => {
-        if (event.key == "Enter") weather.search();
-    });
+document.querySelector(".search-bar").addEventListener("keyup", (event) => {
+  if (event.key == "Enter") weather.search();
+});
 
 // default city
-weather.fetchWeather('isfahan');
+weather.fetchWeather("isfahan");
 
-
-
+// function animate() {
+//   const details = document.querySelectorAll(".details span");
+//   for (let i = 0; i < details.length; i++) {
+//     details[i].style.trasform = "traslateX(-50px)";
+//     setTimeout(() => (details[i].style.trasform = "traslateX(0)"), 500);
+//   }
+// }
--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const loader = document.querySelector("#loading");
 // showing loading
 export function displayLoading() {
-    loader.classList.add("display");
-    // to stop loading after some time
-    setTimeout(() => {
-        loader.classList.remove("display");
-    }, 10000);
+  loader.classList.add("display");
+  // to stop loading after some time
+  setTimeout(() => {
+    loader.classList.remove("display");
+  }, 15000);
 }
 
-// hiding loading 
+// hiding loading
 export function hideLoading() {
-    loader.classList.remove("display");
+  loader.classList.remove("display");
+}
+
+// error message
+export function errorText(message) {
+  const status = document.getElementById("status");
+  status.style.opacity = "1";
+  status.innerHTML = message;
+  setTimeout(() => (status.style.opacity = "0"), 4000);
+}
+
+// export function newDataAnimate() {
+//   const details = document.querySelectorAll(".details span");
+//   for (let i = 0; i < details.length; i++) {
+//     details[i].style.color = "lime";
+//     setTimeout(() => (details[i].style.color = "white"), 500);
+//   }
+// }
+
+export function animateNewData() {
+  const details = document.querySelectorAll(".details > div");
+  for (let i = 0; i < details.length; i++) {
+    details[i].style.opacity = "0.1";
+    setTimeout(() => (details[i].style.opacity = "1"), 400);
+  }
 }
